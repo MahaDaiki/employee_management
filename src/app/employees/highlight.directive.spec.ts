50455618539c5dfc79a880ea1053ec820734f1fe
MahaDaiki <mahadaiki2@gmail.com>
2025-01-03 11:10:30 +0100

    GEA-9  directive highlight pour  highlight des  employees

--- src/app/employees/highlight.directive.spec.ts
+++ src/app/employees/highlight.directive.spec.ts
@@ -1,8 +1,27 @@
 import { HighlightDirective } from './highlight.directive';
+import { ElementRef } from '@angular/core';
 
 describe('HighlightDirective', () => {
+  let directive: HighlightDirective;
+  let mockElementRef: ElementRef;
+
+  beforeEach(() => {
+    mockElementRef = { nativeElement: document.createElement('div') };
+    directive = new HighlightDirective(mockElementRef);
+  });
+
   it('should create an instance', () => {
-    const directive = new HighlightDirective();
     expect(directive).toBeTruthy();
+  });
+
+  it('should apply background color on mouse enter', () => {
+    directive.Highlight = '#cdb38b';
+    directive.onMouseEnter();
+    expect(mockElementRef.nativeElement.style.backgroundColor).toBe('#cdb38b');
+  });
+
+  it('should remove background color on mouse leave', () => {
+    directive.onMouseLeave();
+    expect(mockElementRef.nativeElement.style.backgroundColor).toBe('');
   });
 });
